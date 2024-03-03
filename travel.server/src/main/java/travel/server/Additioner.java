aa3aa7a4432c8160637a9d79af1c3e93681bc18a
YuudiSUN <sunyudi7@gmail.com>
2024-03-03 10:32:32 +0100

    client and service creat

---
+++ travel.server/src/main/java/travel/server/Additioner.java
@@ -0,0 +1,9 @@
+package travel.server;
+
+
+import javax.jws.WebService; import javax.jws.WebMethod; import javax.jws.WebParam;
+@WebService(targetNamespace="http://www.com.computer")
+public class Additioner { @WebMethod
+public int add(@WebParam(name="a") int a, @WebParam(name="b") int b) {
+return a+b; }
+}
